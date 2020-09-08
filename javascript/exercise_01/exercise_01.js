7ba84be643ab0bff46cec6cf0d99ed3851011376
Vishal <vishal205014@gmail.com>
2020-09-08 18:56:50 -0500

    .

---
+++ javascript/exercise_01/exercise_01.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  var age = prompt("Please enter your Age",15);
+  if (age <= 16) {
+    document.getElementById("demo").innerHTML =
+    "redirecting to google.com";
+    window.location.href = "https://www.google.com/";
+  }else{
+    document.getElementById("demo").innerHTML =
+    "your age is "+age; 
+  }
+}
